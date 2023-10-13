00ebc1170b372e85a908ff4a054e83592fbcf1d0
Exegetech <exegetech@protonmail.ch>
2023-10-13 19:23:55 -0400

    Update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -130,6 +130,8 @@ require('lazy').setup({
         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
         vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
       end,
+      -- Disable sign column because it messes up with vim line numbers
+      signcolumn = false,
     },
   },
 
