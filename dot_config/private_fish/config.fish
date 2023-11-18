0cb13c8ca229330c882308c0abcd0356b7cfdc40
Exegetech <exegetech@protonmail.ch>
2023-11-18 11:46:17 -0500

    Updated

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -29,3 +29,21 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# Git aliases
+alias gb="git branch"
+alias gf="git fetch"
+alias gm="git merge"
+alias gs="git status"
+alias gl="git log"
+alias ga="git add"
+alias ga="git add -A"
+alias gc="git commit -m"
+alias gca="git commit --amend --no-edit"
+alias gck="git checkout"
+alias gckb="git checkout -b"
+alias gp="git push"
+alias gpf="git push --force"
+alias grm="git rebase -i master"
+alias save="git add -A && git commit -m 'Updated' && git push origin master"
+
